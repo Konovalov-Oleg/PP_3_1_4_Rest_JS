70bcca361523ecd272829b56e3d1bab97492582e
Konovalov_Oleg <oledjan2014@gmail.com>
2022-08-26 14:34:47 +0300

    commit

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -6,7 +6,10 @@ fetch(requestURL)
     .then(res => res.json())
     .then(data => {
         data.forEach(post => {
-            post.roles.forEach(role => console.log(role.name))
+            const rolesNames = []
+            post.roles.forEach(role => {
+                rolesNames.push(role.name.substring(5))
+            })
             output += `
                 <tr>
                     <td>${post.id}</td>
@@ -14,57 +17,18 @@ fetch(requestURL)
                     <td>${post.lastName}</td>
                     <td>${post.age}</td>
                     <td>${post.email}</td>
-                    <td ${post.roles}>
+                    <td>${rolesNames.join(' ')}</td>
+                    <td>
+                        <!-- Кнопка-триггер модального окна -->
+                        <button type="button" class="btn btn-info" data-toggle="modal"
+                                data-target="${'#editUser' + post.id}">
+                            Edit
+                        </button>
+                        <!-- Модальное окно -->
+                        
                     </td>
-                         
-                    
                 </tr>
                 `
         })
         allUser.innerHTML = output
     })
-
-
-// const requestURL = 'http://localhost:8080/api/users'
-//
-// // GET
-//
-// function sendRequest(method, url, body = null) {
-//     return fetch(url).then(response => {
-//         return response.json()
-//     })
-// }
-//
-// sendRequest('GET', requestURL)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
-
-//POST
-
-// const body = {
-//     firstName: 'Lada',
-//     lastName: 'Konovalova',
-//     age: 17,
-//     email: 'lada@gmail.com',
-//     password: 111,
-//     roles: [
-//         {
-//             id: 1,
-//             name: "ROLE_USER"
-//         },
-//         {
-//             id: 2,
-//             name: "ROLE_ADMIN"
-//         }
-//     ]
-// }
-//
-// fetch(requestURL, {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json;charset=utf-8'
-//     },
-//     body: JSON.stringify(body)
-// })
-//     .then(response => response.json())
-//     .then(result => alert(result.message))
